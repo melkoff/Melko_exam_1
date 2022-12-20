4fe7682731d39d90cbfcca3278c346ce79859a10
melkoff <vasamelko@gmail.com>
2022-12-20 17:59:06 +0200

    header-done

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,21 @@ let slider = $(document).ready(function () {
   });
 });
 
+//burger menu
+'use strict';
+const humb = document.querySelector('#burger');
+const humbBody = document.querySelector('#burger-menu');
+const header = document.querySelector('.header');
+const menuOpen = document.querySelector('html');
+
+
+humb.addEventListener('click', humbHandler);
+
+function humbHandler(e) {
+  e.preventDefault();
+  humbBody.classList.toggle("open");
+  menuOpen.classList.toggle("menu__open");
+  humb.classList.toggle('active');
+  header.classList.toggle('active');
+  window.scrollTo(0, 0);
+}
