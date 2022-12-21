b9fba7bab3f393351381384a5fb487dce53d44ac
melkoff <vasamelko@gmail.com>
2022-12-21 17:03:17 +0200

    add story

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 let slider = $(document).ready(function () {
-  $('.your-class').slick({
-    slidesToShow: 1,
+  $('.gallery__slider').slick({
+    slidesToShow: 4,
     slidesToScroll: 1,
     arrows: true,
     dots: true,
