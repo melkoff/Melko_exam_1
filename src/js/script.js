ae7a819c6064740530ed321f5b46b63ce6719a4d
melkoff <vasamelko@gmail.com>
2022-12-28 22:43:08 +0200

    added accordion and fixed scroll to top

--- src/js/script.js
+++ src/js/script.js
@@ -62,3 +62,20 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+//Accordion
+
+var acc = document.getElementsByClassName("btn btn-outline-secondary");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
